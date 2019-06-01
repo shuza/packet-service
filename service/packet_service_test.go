36f46e6e0977ef40eb0eb0c9973c6fabe7c000c2
Shuza <shuza.sa@gmail.com>
2019-06-01 16:18:11 +0600

    test case added for packet service

--- service/packet_service_test.go
+++ service/packet_service_test.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"github.com/shuza/packet-service/db"
 	pb "github.com/shuza/packet-service/proto"
 	. "github.com/smartystreets/goconvey/convey"
@@ -27,9 +28,8 @@ func TestService_CreatePacket(t *testing.T) {
 
 	repo := &db.MockDb{}
 	repo.Mock.On("Create", packet).Return(packet, nil)
-
 	packetService := NewPacketService(repo)
-	Convey("Requesting to create a new packet", t, func() {
+	Convey("Requesting to create a new valid packet", t, func() {
 		resp, err := packetService.CreatePacket(context.Background(), packet)
 		Convey("Check response and error", func() {
 			So(err, ShouldBeNil)
@@ -37,6 +37,43 @@ func TestService_CreatePacket(t *testing.T) {
 		})
 	})
 
+	repo = &db.MockDb{}
+	repo.Mock.On("Create", packet).Return(packet, errors.New("Can't create packet"))
+	packetService = NewPacketService(repo)
+	Convey("Requesting to create an invalid packet", t, func() {
+		resp, err := packetService.CreatePacket(context.Background(), packet)
+		Convey("Should fail the operation", func() {
+			So(err, ShouldNotBeNil)
+			So(resp, ShouldBeNil)
+		})
+	})
+}
+
+func TestService_GetPackets(t *testing.T) {
+	packet, err := parseFile("./../packet-cli/packet.json")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	repo := &db.MockDb{}
+	repo.Mock.On("Create", packet).Return(packet, nil)
+	packetService := NewPacketService(repo)
+	Convey("Requesting for empty packet list", t, func() {
+		resp, err := packetService.GetPackets(context.Background(), &pb.Empty{})
+		Convey("It should return empty packet list", func() {
+			So(err, ShouldBeNil)
+			So(len(resp.Packets), ShouldBeZeroValue)
+		})
+	})
+
+	_, _ = packetService.CreatePacket(context.Background(), packet)
+	Convey("Requesting for packet list", t, func() {
+		resp, err := packetService.GetPackets(context.Background(), &pb.Empty{})
+		Convey("It should return one packet in the list", func() {
+			So(err, ShouldBeNil)
+			So(len(resp.Packets), ShouldEqual, 1)
+		})
+	})
 }
 
 func parseFile(file string) (*pb.Packet, error) {
